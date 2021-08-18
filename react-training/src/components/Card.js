6c6bc6fad9868a7ca8add49741c1c35fec9bdc73
DHorb <darya.grob@gmail.com>
2021-08-18 21:11:45 +0300

    task1

---
+++ react-training/src/components/Card.js
@@ -0,0 +1,17 @@
+import './Card.css'
+function  Card() {
+    return (
+        <div className="card cardStyle">
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+    );
+
+
+
+
+}
+export default Card;
