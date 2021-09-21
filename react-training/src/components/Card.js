0b1873c6dff3e9119e0fafaf2513faa1078abc95
DHorb <darya.grob@gmail.com>
2021-09-21 17:34:53 +0300

    task3-fixed

--- react-training/src/components/Card.js
+++ react-training/src/components/Card.js
@@ -1,7 +1,6 @@
 import "./Card.css"
 import React from "react";
-import { AiOutlineEdit } from "react-icons/ai";
-import EditCard from "./EditCard";
+import {AiOutlineClose, AiOutlineEdit, AiOutlineSave} from "react-icons/ai";
 
 class Card extends React.Component {
 
@@ -11,12 +10,20 @@ class Card extends React.Component {
             showing: false,
             currentTitle: "Card title",
             currentText: "Text",
-
+            titleCount: 0,
+            textCount:0,
+            prevTitle: "Card title",
+            prevText: "text"
         };
         this.hideCheckBoxChange = this.hideCheckBoxChange.bind(this);
         this.setCurrentText = this.setCurrentText.bind(this);
         this.setCurrentTitle = this.setCurrentTitle.bind(this);
         this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+        this.save = this.save.bind(this);
+        this.cancel = this.cancel.bind(this);
+        this.changeText = this.changeText.bind(this);
+        this.changeTitle = this.changeTitle.bind(this);
+
     }
 
     render() {
@@ -29,9 +36,12 @@ class Card extends React.Component {
         return (
             <div className={classes.join(" ")}>
                 <div className="card-body">
-                    <EditCard showing={!this.state.showing} hide={this.hideCheckBoxChange}
-                        setCurrentText={this.setCurrentText} setCurrentTitle={this.setCurrentTitle}
-                        currentTitle={this.state.currentTitle} currentText={this.state.currentText}/>
+                    <div hidden={!this.state.showing}>
+                        <input type="text" size="40px" value={this.state.currentTitle} onChange={this.changeTitle}/>
+                        <button className="saveButton" onClick={this.save}><AiOutlineSave/></button>
+                        <button className="closeButton" onClick={this.cancel}><AiOutlineClose/></button>
+                        <textarea value={this.state.currentText} onChange={this.changeText}/>
+                    </div>
                     <div className="cardTitle" hidden={this.state.showing}>
                         <div className="title">
                             <h5 style={{paddingTop: "3px"}}>{this.state.currentTitle}</h5>
@@ -62,6 +72,36 @@ class Card extends React.Component {
     hideCheckBoxChange() {
         this.setState({showing: !this.state.showing});
         this.setState({checked: false});
+    }
+
+    save() {
+        this.setState({titleCount: 0});
+        this.setState({textCount: 0});
+        this.setState({prevText: this.state.currentText});
+        this.setState({prevTitle: this.state.currentTitle});
+        this.hideCheckBoxChange();
+    }
+
+    changeTitle(event) {
+        if(this.state.titleCount === 0){
+            this.setState({prevTitle: this.state.currentTitle})
+        }
+        this.setState({currentTitle: event.target.value})
+        this.setState({titleCount: this.state.titleCount + 1})
+    }
+
+    changeText(event) {
+        if(this.state.textCount === 0){
+            this.setState({prevText: this.state.currentText})
+        }
+        this.setState({currentText: event.target.value});
+        this.setState({textCount: this.state.textCount + 1});
+    }
+
+    cancel() {
+        this.hideCheckBoxChange();
+        this.setState({currentText: this.state.prevText});
+        this.setState({currentTitle: this.state.prevTitle});
     }
 
 }
