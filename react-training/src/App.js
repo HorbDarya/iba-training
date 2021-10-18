859335b40e883b78f9c57f77cccdcbaa6615396d
DHorb <darya.grob@gmail.com>
2021-10-18 13:40:02 +0300

    task4

--- react-training/src/App.js
+++ react-training/src/App.js
@@ -1,12 +1,94 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from "./components/Card";
 import React from 'react';
-
+import CardList from "./components/CardList";
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            isEditable: false,
+            cards: [
+                {
+                    id: 1,
+                    showing: false,
+                    currentTitle: "Card title1",
+                    currentText: "Text1",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title1",
+                    prevText: "Text1"
+                },
+                {
+                    id: 2,
+                    showing: false,
+                    currentTitle: "Card title2",
+                    currentText: "Text2",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title2",
+                    prevText: "Text2"
+                },
+                {
+                    id: 3,
+                    showing: false,
+                    currentTitle: "Card title3",
+                    currentText: "Text3",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title3",
+                    prevText: "Text3"
+                },
+                {
+                    id: 4,
+                    showing: false,
+                    currentTitle: "Card title4",
+                    currentText: "Text4",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title4",
+                    prevText: "Text4"
+                },
+                {
+                    id: 5,
+                    showing: false,
+                    currentTitle: "Card title5",
+                    currentText: "Text5",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title5",
+                    prevText: "Text5"
+                },
+                {
+                    id: 6,
+                    showing: false,
+                    currentTitle: "Card title6",
+                    currentText: "Text6",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title6",
+                    prevText: "Text6"
+                },
+                {
+                    id: 7,
+                    showing: false,
+                    currentTitle: "Card title7",
+                    currentText: "Text7",
+                    titleCount: 0,
+                    textCount:0,
+                    prevTitle: "Card title7",
+                    prevText: "Text7"
+                },
+            ]
+        };
+
+        this.hideCheckBoxChange = this.hideCheckBoxChange.bind(this);
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+        this.save = this.save.bind(this);
+        this.cancel = this.cancel.bind(this);
+        this.changeText = this.changeText.bind(this);
+        this.changeTitle = this.changeTitle.bind(this);
+        this.handleCheckbox = this.handleCheckbox.bind(this);
     }
 
     render() {
@@ -14,12 +96,93 @@ class App extends React.Component {
             <div className="container">
                 <div className="jumbotron">
                     <h1 className="display-3">App Name</h1>
-                </div>
-                <div>
-                    <Card state={this.state} th={this}></Card>
+                    <label><input type="checkbox" checked={this.state.isEditable} onChange={this.handleCheckbox}/>Read only</label>
+                    <CardList cards={this.state.cards} changeTitle={this.changeTitle} save={this.save}
+                        hideCheckBoxChange={this.hideCheckBoxChange} cancel={this.cancel} changeText={this.changeText}
+                        handleCheckboxChange={this.handleCheckboxChange} isEditable={this.state.isEditable}/>
                 </div>
             </div>
         );
+    }
+
+    handleCheckboxChange(id) {
+        this.setState({cards: this.state.cards.map(card => {
+            if(card.id === id){
+                card.checked = !card.checked;
+            }
+            return card;
+        })})
+    }
+
+    handleCheckbox() {
+        this.setState({isEditable: !this.state.isEditable});
+        this.state.cards.map(card => {
+            if(card.showing === true){
+                this.cancel(card.id);
+            }
+        })
+    }
+
+    hideCheckBoxChange(id) {
+        this.setState({cards: this.state.cards.map(card => {
+            if(card.id === id){
+                card.showing = !card.showing;
+                card.checked = false;
+            }
+            return card;
+        })})
+    }
+
+    save(id) {
+        this.setState({cards: this.state.cards.map(card => {
+            if(card.id === id){
+                card.titleCount = 0;
+                card.textCount = 0;
+                card.prevTitle = card.currentTitle;
+                card.prevText = card.currentText;
+                this.hideCheckBoxChange(id);
+            }
+            return card;
+        })})
+    }
+
+    changeTitle(event, id) {
+        this.setState({cards: this.state.cards.map(card => {
+            if(card.id === id){
+                if(card.titleCount === 0){
+                    card.prevTitle = card.currentTitle;
+                }
+                card.currentTitle = event.target.value;
+                card.titleCount = card.titleCount + 1;
+            }
+            return card;
+        })})
+    }
+
+    changeText(event, id) {
+        this.setState({cards: this.state.cards.map(card => {
+            if(card.id === id){
+                if(card.textCount === 0){
+                    card.prevText = card.currentText;
+                }
+
+                card.currentText = event.target.value;
+                card.textCount = card.textCount + 1;
+            }
+            return card;
+        })})
+    }
+
+    cancel(id) {
+        this.hideCheckBoxChange(id);
+        this.setState({cards: this.state.cards.map(card => {
+            if(card.id === id){
+                card.currentText = card.prevText;
+                card.currentTitle = card.prevTitle;
+            }
+            return card;
+        })})
+
     }
 }
 
--- react-training/src/components/Card.js
+++ react-training/src/components/Card.js
@@ -1,107 +1,52 @@
 import "./Card.css"
 import React from "react";
 import {AiOutlineClose, AiOutlineEdit, AiOutlineSave} from "react-icons/ai";
+import cn from "classnames";
 
 class Card extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            showing: false,
-            currentTitle: "Card title",
-            currentText: "Text",
-            titleCount: 0,
-            textCount:0,
-            prevTitle: "Card title",
-            prevText: "text"
-        };
-        this.hideCheckBoxChange = this.hideCheckBoxChange.bind(this);
-        this.setCurrentText = this.setCurrentText.bind(this);
-        this.setCurrentTitle = this.setCurrentTitle.bind(this);
-        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
-        this.save = this.save.bind(this);
-        this.cancel = this.cancel.bind(this);
-        this.changeText = this.changeText.bind(this);
-        this.changeTitle = this.changeTitle.bind(this);
-
-    }
-
     render() {
-        const classes = ["cardStyle", "card"]
+        const classNames = cn(
+            "cardStyle", "card", {
+                "marked": this.props.card.checked
+            }
+         );
 
-        if(this.state.checked){
-            classes.push("marked")
-        }
-
+        const visibility = cn(
+            "editButton", {
+                "non-visible": this.props.isEditable
+            }
+        );
+        const { card } = this.props;
         return (
-            <div className={classes.join(" ")}>
+
+            <div className={classNames}>
                 <div className="card-body">
-                    <div hidden={!this.state.showing}>
-                        <input type="text" size="40px" value={this.state.currentTitle} onChange={this.changeTitle}/>
-                        <button className="saveButton" onClick={this.save}><AiOutlineSave/></button>
-                        <button className="closeButton" onClick={this.cancel}><AiOutlineClose/></button>
-                        <textarea value={this.state.currentText} onChange={this.changeText}/>
+                    <div hidden={!card.showing}>
+                        <input type="text" size="40px" value={card.currentTitle}
+                               onChange={(event)=>this.props.changeTitle(event, this.props.card.id)}/>
+                        <button className="saveButton"
+                                onClick={()=>this.props.save(card.id)} hidden={this.props.isEditable}><AiOutlineSave/></button>
+                        <button className="closeButton"
+                                onClick={()=>this.props.cancel(card.id)} hidden={this.props.isEditable}><AiOutlineClose/></button>
+                        <textarea value={card.currentText}
+                                  onChange={(event)=>this.props.changeText(event, card.id)}/>
                     </div>
-                    <div className="cardTitle" hidden={this.state.showing}>
+                    <div className="cardTitle" hidden={card.showing}>
                         <div className="title">
-                            <h5 style={{paddingTop: "3px"}}>{this.state.currentTitle}</h5>
+                            <h5 style={{paddingTop: "3px"}}>{card.currentTitle}</h5>
                         </div>
-                        <button className="editButton" onClick={this.hideCheckBoxChange}> <AiOutlineEdit/> </button>
-                        <input type="checkbox" checked={this.state.checked} onChange={this.handleCheckboxChange}/>
+                        <button className={visibility}
+                                onClick={()=>this.props.hideCheckBoxChange(card.id)} > <AiOutlineEdit/></button>
+                        <input type="checkbox" id="changeColour" checked={card.checked}
+                                    onChange={()=>this.props.handleCheckboxChange(card.id)}/>
                     </div>
                     <div className="card-text">
-                        <p hidden={this.state.showing}>{this.state.currentText}</p>
+                        <p hidden={card.showing}>{card.currentText}</p>
                     </div>
                 </div>
             </div>
         );
-    }
-
-    setCurrentText(text){
-        this.setState({currentText: text});
-    }
-
-    setCurrentTitle(text){
-        this.setState({currentTitle: text});
-    }
-
-    handleCheckboxChange() {
-        this.setState({checked: !this.state.checked});
-    }
-
-    hideCheckBoxChange() {
-        this.setState({showing: !this.state.showing});
-        this.setState({checked: false});
-    }
-
-    save() {
-        this.setState({titleCount: 0});
-        this.setState({textCount: 0});
-        this.setState({prevText: this.state.currentText});
-        this.setState({prevTitle: this.state.currentTitle});
-        this.hideCheckBoxChange();
-    }
-
-    changeTitle(event) {
-        if(this.state.titleCount === 0){
-            this.setState({prevTitle: this.state.currentTitle})
-        }
-        this.setState({currentTitle: event.target.value})
-        this.setState({titleCount: this.state.titleCount + 1})
-    }
-
-    changeText(event) {
-        if(this.state.textCount === 0){
-            this.setState({prevText: this.state.currentText})
-        }
-        this.setState({currentText: event.target.value});
-        this.setState({textCount: this.state.textCount + 1});
-    }
-
-    cancel() {
-        this.hideCheckBoxChange();
-        this.setState({currentText: this.state.prevText});
-        this.setState({currentTitle: this.state.prevTitle});
     }
 
 }
